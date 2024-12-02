240d455304e38c527af9ff7f352b4ad78dccd5b7
revathylilly <revathylilly@gmail.com>
2024-12-02 13:50:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base:"/Pothichoru",
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
